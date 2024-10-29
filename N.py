50f85d74129abb3829807621f29b12bcb3eedcd5
NicolasPalma05 <nicolaspalma2005@gmail.com>
2024-10-29 16:46:06 -0300

    Update N.py

--- N.py
+++ N.py
@@ -1,13 +1,26 @@
 import streamlit as st
-st.title('Manchester City Football Club')
-st.image('equipo.jpg')
-st.sidebar.title('Ubicacion')
-st.sidebar.image('reino unido.jpg')
-st.sidebar.write('Manchester, Reino Unido')
-st.text('WINER UEFA CHAMPIONS LEAGUE 2022/2023')
-st.sidebar.title('Estadio')
-st.sidebar.text('Etihad Stadium')
-st.sidebar.image('city.jpg')
-st.sidebar.title('Entrenador')
-st.sidebar.image('guardiola.jpg')
-st.sidebar.text(' Pep Guardiola ')
+
+Plantilla = {
+  'Equipo 1' = 'P1'
+  'Equipo 2' = 'P2'
+}
+
+select = st.sidebar.selectbox('Plantilla:',list(Plantilla.keys())))
+
+if select = 'Equipo 1':
+def a():
+  st.title('Manchester City Football Club')
+  st.image('equipo.jpg')
+  st.sidebar.title('Ubicacion')
+  st.sidebar.image('reino unido.jpg')
+  st.sidebar.write('Manchester, Reino Unido')
+  st.text('WINER UEFA CHAMPIONS LEAGUE 2022/2023')
+  st.sidebar.title('Estadio')
+  st.sidebar.text('Etihad Stadium')
+  st.sidebar.image('city.jpg')
+  st.sidebar.title('Entrenador')
+  st.sidebar.image('guardiola.jpg')
+  st.sidebar.text(' Pep Guardiola ')
+elif select = 'Equipo 2':
+  import N2
+  N2.Suplentes()
