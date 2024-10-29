36fdcb4d32fb8442f438566dd4e5c59f38636afb
NicolasPalma05 <nicolaspalma2005@gmail.com>
2024-10-29 15:42:57 -0300

    Update N.py

--- N.py
+++ N.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('Aplicacion')
-st.sidebar.title('Aplicacion')
-st.text('holas')
+st.title('Manchester City')
+st.sidebar.title('Manchester City')
+st.text('aaaaaaaaaaaaaaa')
 st.image('city.jpg')
