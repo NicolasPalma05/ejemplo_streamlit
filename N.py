8ec3b3ef7db673d022c92259cc0c03197966cbb6
NicolasPalma05 <nicolaspalma2005@gmail.com>
2024-10-29 16:06:01 -0300

    Update N.py

--- N.py
+++ N.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.title('Manchester City Football Club')
 st.sidebar.title('Manchester City')
 st.sidebar.title('Ubicacion')
-st.sitebar.write('manchester, Reino Unido')
+st.sidebar.write('manchester, Reino Unido')
 st.text('Premier League')
 st.image('city.jpg')
