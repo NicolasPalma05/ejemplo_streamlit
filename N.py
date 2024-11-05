20d1d428be3e9165cb9fabce3b8008b22fd8f63f
NicolasPalma05 <nicolaspalma2005@gmail.com>
2024-11-05 15:54:32 -0300

    Update N.py

--- N.py
+++ N.py
@@ -13,6 +13,7 @@ if select == 'HOME':
   st.sidebar.image('reino unido.jpg')
   st.sidebar.write('Manchester, Reino Unido')
   st.text('WINER UEFA CHAMPIONS LEAGUE 2022/2023')
+  st.image('Manchester City.avif')
   st.sidebar.title('Estadio')
   st.sidebar.text('Etihad Stadium')
   st.sidebar.image('city.jpg')
