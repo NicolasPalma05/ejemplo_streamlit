9a113ae4c6a50bd7d00a3c145c0c8b0e63625336
NicolasPalma05 <nicolaspalma2005@gmail.com>
2024-11-15 11:41:36 -0300

    Update N.py

--- N.py
+++ N.py
@@ -26,3 +26,33 @@ elif select == 'statistics':
 elif select == 'Grafico':
   import N3
   N3.grafico()
+
+
+
+
+
+
+
+
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+    st.session_state.horizontal = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable radio widget", key="disabled")
+    st.checkbox("Orient radio options horizontally", key="horizontal")
+
+with col2:
+    st.radio(
+        "Set label visibility 👇",
+        ["visible", "hidden", "collapsed"],
+        key="visibility",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        horizontal=st.session_state.horizontal,
+    )
